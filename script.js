1e923110d17f670ced37a18952ced0d314dba845
Ali Al-Dhib <alifahed67@gmail.com>
2022-07-20 21:47:32 +0800

    Add script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const previousCalculation = document.querySelector('[data-previous-calculation]');
+const currentCalculation = document.querySelector('[data-current-calculation]');
+const acButton = document.querySelector('[data-ac]');
+const clearButton = document.querySelector('[data-clear]');
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelector('[data-operator]');
+const equalsbutton = document.querySelector('[data-equals]');
